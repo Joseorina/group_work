5baabadfe9a7a139ce74486e804602051fac97bb
Joseph Orina <orinangare@gmail.com>
2019-07-24 19:02:10 +0300

    [Design of stack implementation using a linked list]

---
+++ linked_stackk.py
@@ -0,0 +1,26 @@
+class LinkedStack:
+    """
+    Stack implementation using a singly linked list for storage
+    """
+            
+    class _Node:
+        def __init__(self):
+            pass
+    
+    def __init__(self):
+        pass
+    
+    def __len__(self):
+        pass
+    
+    def check_empty_stack(self):
+        pass
+    
+    def push(self, element):
+        pass
+    
+    def pop(self, element):
+        pass
+    
+    def peek(self):
+        pass
